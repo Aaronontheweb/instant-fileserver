ad9420daa8c5b1236a6d31a7c475ec2b891f0784
Aaron Stannard <aaron@markedup.com>
2013-08-13 01:34:22 -0700

    restructuring our package to make it deployable as a binary

---
+++ bin/ifs.js
@@ -0,0 +1,9 @@
+/*
+ * file: 			ifs.js
+ * description: 	designed to make the instant-server command line instantiable
+ * author: 			Aaron Stannard
+ * created: 		8/12/2013
+ * last-modified: 	8/12/2013
+ */
+
+ require('../lib/app');
