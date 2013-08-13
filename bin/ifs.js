1cf4abb1c7ee5cfacb9a471ac81f9283eb773d81
Aaron Stannard <aaron@markedup.com>
2013-08-13 12:25:39 -0700

    working on getting bin to work on Linux

--- bin/ifs.js
+++
@@ -1,9 +0,0 @@
-/*
- * file: 			ifs.js
- * description: 	designed to make the instant-server command line instantiable
- * author: 			Aaron Stannard
- * created: 		8/12/2013
- * last-modified: 	8/12/2013
- */
-
- require('../lib/app');
